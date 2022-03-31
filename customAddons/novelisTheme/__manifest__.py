be0e89b62e88c6c3923c03ae17801ec535494de2
amanie <amanie@novelis.io>
2022-03-31 11:54:55 +0000

    change form input style and module name

--- customAddons/novelisTheme/__manifest__.py
+++ customAddons/novelisTheme/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name' : 'custom module (ayoub.2)',
+    'name' : 'novelis theme',
     'version' : '1.0',
     'summary': 'custom',
     'sequence': 10,
@@ -17,11 +17,11 @@
     'auto_install': False,
     'assets': {
         'web.assets_backend': [
-            # 'customModule/static/src/css/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2',
+            # 'novelisTheme/static/src/css/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2',
             
-            ("before","web/static/src/legacy/scss/primary_variables.scss",'customModule/static/src/css/colors.scss'),
-            ("after","web/static/src/legacy/scss/utils.scss",'customModule/static/src/css/utils.scss'),
-            'customModule/static/src/css/custom-odoo-css.scss',
+            ("before","web/static/src/legacy/scss/primary_variables.scss",'novelisTheme/static/src/css/colors.scss'),
+            ("after","web/static/src/legacy/scss/utils.scss",'novelisTheme/static/src/css/utils.scss'),
+            'novelisTheme/static/src/css/main.scss'
         ],
     }
 }
